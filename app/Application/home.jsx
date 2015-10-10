235f0817ff3f87f51e24e8ec9dc9fcd4f1af9b1c
meta-meta <paulmchristian@gmail.com>
2015-10-10 11:52:54 -0500

    experiment

--- app/Application/home.jsx
+++ app/Application/home.jsx
@@ -4,6 +4,33 @@ let ImmutableOptimizations = require('react-cursor').ImmutableOptimizations;
 let _ = require('lodash');
 let Panel = require('react-bootstrap').Panel;
 
+let Comment = React.createClass({
+
+	render() {
+		let comment = this.props.comment;
+
+		return <div className='comment-container'>
+			<div className='comment color1'>
+				<h3>{comment.author}</h3>
+				<p>{comment.data.text}</p>
+			</div>
+			{comment.children ? <Comments comments={comment.children}/> : null}
+		</div>
+	}
+});
+
+let Comments = React.createClass({
+	render() {
+		let comments = this.props.comments;
+
+		return <div className={'comments ' + this.props.className}>
+			{_.map(comments, comment => <Comment comment={comment} />)}
+
+
+		</div>
+	}
+});
+
 let Home = React.createClass({
 
 	contextTypes: {
@@ -14,12 +41,14 @@ let Home = React.createClass({
 
 	render() {
 		let cursor = this.props.cursor.refine('account');
+		let data = this.props.cursor.refine('data').value;
 
 		let url = cursor.refine('imageUrl').value;
 
 		return <Panel className="container home main-content">
 			<div className="avatar" style={url ? {backgroundImage: `url('${url}')`} : null} />
 			<h1>Welcome back, {cursor.refine('name').value}</h1>
+			<Comments className='outermost' comments={data} />
 		</Panel>;
 	}
 });
--- app/Application/initialState.jsx
+++ app/Application/initialState.jsx
@@ -20,7 +20,136 @@ const InitialState = {
 
     ui: {},
 
-    data: {},
+    data: {
+        12345: {
+            id: 12345,
+            author: 'Paul',
+            data: {
+                text: 'a comment. this is a comment that is going to be the root node. all other comments are in this thread'
+            },
+            rating: 0,
+            created: new Date(),
+
+            children: {
+                23456: {
+                    id: 23456,
+                    author: 'Matt',
+                    data: {
+                        text: 'a reply to a comment'
+                    },
+                    rating: 0,
+                    created: new Date(),
+
+                    children: {
+                        45678: {
+                            id: 45678,
+                            author: 'Paul',
+                            data: {
+                                text: 'a reply to Matt\'s reply'
+                            },
+                            rating: 0,
+                            created: new Date(),
+
+                            children: {
+                                45678: {
+                                    id: 45678,
+                                    author: 'Paul',
+                                    data: {
+                                        text: 'a reply to Matt\'s reply'
+                                    },
+                                    rating: 0,
+                                    created: new Date(),
+
+                                    children: {}
+                                },
+                                56789: {
+                                    id: 56789,
+                                    author: 'Kaya',
+                                    data: {
+                                        text: 'a reply to Matt\'s reply'
+                                    },
+                                    rating: 0,
+                                    created: new Date(),
+
+                                    children: {
+                                        45678: {
+                                            id: 45678,
+                                            author: 'Paul',
+                                            data: {
+                                                text: 'a reply to Matt\'s reply'
+                                            },
+                                            rating: 0,
+                                            created: new Date(),
+
+                                            children: {}
+                                        },
+                                        56789: {
+                                            id: 56789,
+                                            author: 'Kaya',
+                                            data: {
+                                                text: 'a reply to Matt\'s reply'
+                                            },
+                                            rating: 0,
+                                            created: new Date(),
+
+                                            children: {
+                                                45678: {
+                                                    id: 45678,
+                                                    author: 'Paul',
+                                                    data: {
+                                                        text: 'a reply to Matt\'s reply'
+                                                    },
+                                                    rating: 0,
+                                                    created: new Date(),
+
+                                                    children: {}
+                                                },
+                                                56789: {
+                                                    id: 56789,
+                                                    author: 'Kaya',
+                                                    data: {
+                                                        text: 'a reply to Matt\'s reply'
+                                                    },
+                                                    rating: 0,
+                                                    created: new Date(),
+
+                                                    children: {}
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        56789: {
+                            id: 56789,
+                            author: 'Kaya',
+                            data: {
+                                text: 'a reply to Matt\'s reply'
+                            },
+                            rating: 0,
+                            created: new Date(),
+
+                            children: {}
+                        }
+                    }
+                },
+
+                34567: {
+                    id: 34567,
+                    author: 'Adam',
+                    data: {
+                        text: 'a different reply to a comment'
+                    },
+                    rating: 0,
+                    created: new Date(),
+
+                    children: {}
+                }
+
+            }
+        }
+    },
 
     forms: {
         options: {
